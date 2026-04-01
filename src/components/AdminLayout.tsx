e19636a94f8ac3d45949c3bd027006b3da83668f
optmus825454 <ikinciyenikitap54@gmail.com>
2026-04-02 01:29:36 +0300

    update: AdminLayout.tsx

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
